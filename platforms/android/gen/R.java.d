5f69961857a892f1c55fd43a504915ff79070012
xiefei <xiefei@gtan.com>
2013-11-01 16:34:08 +0800

    add needed parts in platforms/ directory

---
+++ platforms/android/gen/R.java.d
@@ -0,0 +1,10 @@
+/home/xf/projects/DrGuoDemo/platforms/android/gen/com/gtan/demo/drguo/R.java \
+ : /home/xf/projects/DrGuoDemo/platforms/android/res/drawable/icon.png \
+/home/xf/projects/DrGuoDemo/platforms/android/res/xml/config.xml \
+/home/xf/projects/DrGuoDemo/platforms/android/res/drawable-xhdpi/icon.png \
+/home/xf/projects/DrGuoDemo/platforms/android/res/drawable-xhdpi/splash.png \
+/home/xf/projects/DrGuoDemo/platforms/android/res/values/strings.xml \
+/home/xf/projects/DrGuoDemo/platforms/android/res/drawable-hdpi/icon.png \
+/home/xf/projects/DrGuoDemo/platforms/android/res/drawable-mdpi/icon.png \
+/home/xf/projects/DrGuoDemo/platforms/android/res/drawable-ldpi/icon.png \
+/home/xf/projects/DrGuoDemo/platforms/android/bin/AndroidManifest.xml \
---
+++ platforms/android/gen/com/gtan/demo/drguo/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.gtan.demo.drguo;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
